04a6aacaa4615f8b258dbccda22c12d9a8b563cb
mjbright <github.com@mjbright.net>
2016-02-02 15:00:46 +0100

    Added first example - build a statically compiled binary from C

---
+++ createTinyC/createTinyDockerImage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash  
+
+TAG=lab/c_prog
+
+echo; echo "-- Build helloWorld.exe"
+gcc -static helloWorld.c -o helloWorld.exe
+ls -al helloWorld.exe
+
+ldd "helloWorld.exe"
+
+echo; echo "-- Build the docker image <$TAG>"
+docker build -t $TAG ./
+
+echo; echo "-- docker images $TAG"
+docker images $TAG
+
+echo; echo "-- docker run $TAG"
+docker run $TAG
+
