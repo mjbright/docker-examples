f0db6f2fa344df3a5ff5644c82d91f74f7cbc04e
Mike Bright <github.com@mjbright.net>
2016-02-03 14:47:35 +0100

    Corrected dumb typo

--- START_LAB.sh
+++ START_LAB.sh
@@ -5,7 +5,7 @@ SRC_DIR=${0%/*}
 DEST_DIR=/tmp/initial_${DIR}
 
 
-[ ! -d /tmp/initial_${DIR} } &&
+[ ! -d /tmp/initial_${DIR} ] &&
     rsync -a ${SRC_DIR} ${DEST_DIR}
 
 find $DIR/ -name 'docker-compose.yml*' -exec rm {} \;
