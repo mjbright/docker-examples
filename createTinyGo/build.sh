ee0c18fd2648f5bf5d6786a4730df77215af5f30
Mike Bright <github.com@mjbright.net>
2016-02-03 14:11:20 +0100

    Modified to create 2 images, go-hello and go-web

--- createTinyGo/build.sh
+++ createTinyGo/build.sh
@@ -4,5 +4,9 @@ docker run -it -v $PWD:/go golang go build -v hello
 
 docker run -it -v $PWD:/go golang go build -v webserver
 
-docker build -t lab/go-hello .
+docker build -t lab/go-hello -f Dockerfile.hello .
+
+docker build -t lab/go-web -f Dockerfile .
+
+
 
