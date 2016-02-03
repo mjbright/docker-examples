87acd27ffffacc729b9e57868a45c7833b2cf8be
mjbright <github.com@mjbright.net>
2016-02-03 14:47:54 +0100

    Script to prepare lab execution - remove the answers ... (lightly)

---
+++ START_LAB.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SRC_DIR=${0%/*}
+
+DEST_DIR=/tmp/initial_${DIR}
+
+
+[ ! -d /tmp/initial_${DIR} } &&
+    rsync -a ${SRC_DIR} ${DEST_DIR}
+
+find $DIR/ -name 'docker-compose.yml*' -exec rm {} \;
+find $DIR/ -name 'Dockerfile*'         -exec rm {} \;
+
+
+
+
