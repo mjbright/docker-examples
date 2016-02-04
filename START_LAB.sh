bf08676cd04e5b6fd59349a0fb66f2c6bdb70a05
mjbright <github.com@mjbright.net>
2016-02-04 07:06:22 +0100

    Remove build*.sh files also at start of lab

--- START_LAB.sh
+++ START_LAB.sh
@@ -12,6 +12,7 @@ DEST_DIR=/tmp/initial_${DIR}
 
 find $SRC_DIR/ -iname 'docker-compose.yml*' -exec rm {} \;
 find $SRC_DIR/ -iname 'Dockerfile*'         -exec rm {} \;
+find $SRC_DIR/ -iname '*build*.sh'          -exec rm {} \;
 
 
 
