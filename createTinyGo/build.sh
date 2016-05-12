d5af66f85eb0279c01d46eac3d816cbfbac61038
Mike Bright <github@mjbright.net>
2016-05-12 17:11:51 +0200

    Made build script more verbose for demos

--- createTinyGo/build.sh
+++ createTinyGo/build.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
 
-docker run -it -v $PWD:/go golang go build -v hello
+press() {
+    echo $*
+    echo "Press <return> to continue"
+    read _DUMMY
+    [ "$_DUMMY" = "q" ] && exit 0
+    [ "$_DUMMY" = "Q" ] && exit 0
+}
 
-docker run -it -v $PWD:/go golang go build -v webserver
+docker images | grep ^lab/
+IDS=$(docker images | grep ^lab/ | awk '{print $1;}')
+if [ ! -z "$IDS" ];then
+    press "Cleaning up old images"
+    docker rmi $IDS
+fi
 
-docker build -t lab/go-hello -f Dockerfile.hello .
+echo
+press "About to build exectuables using Go-compiler in 'golang' container"
+CMD="docker run -it -v $PWD:/go golang go build -v hello"
+echo $CMD; $CMD
+echo "----"
+ls -alh hello
 
-docker build -t lab/go-web -f Dockerfile .
+CMD="docker run -it -v $PWD:/go golang go build -v webserver"
+echo $CMD; $CMD
+echo "----"
+ls -alh webserver
+
+echo
+press "About to build our own images including these new binaries"
+echo
+echo "-- Building image including hello binary"
+CMD="docker build -t lab/go-hello -f Dockerfile.hello ."
+echo $CMD; $CMD
+
+echo
+echo "-- Building image including webserver binary"
+CMD="docker build -t lab/go-web -f Dockerfile.webserver ."
+echo $CMD; $CMD
+
+echo
+docker images | grep ^lab/
 
 
 
